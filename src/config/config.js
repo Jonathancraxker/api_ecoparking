bd3a847cd73b45a68e9098858635360d5e86543b
Jonathancraxker <Jonathancraxkerssj@gmail.com>
2025-10-15 01:06:16 -0600

    Eliminación de credenciales

--- src/config/config.js
+++ src/config/config.js
@@ -4,9 +4,9 @@ config();
 
 export const PORT = process.env.PORT || 4000
 
-export const DB_USER = process.env.DB_USER || "u835344296_rubenrivera"
-export const DB_PASSWORD = process.env.DB_PASSWORD || "Bdc13Nqr04dm7ns"
-export const DB_HOST = process.env.DB_HOST || "srv868.hstgr.io"
-export const DB_DATABASE = process.env.DB_DATABASE || "u835344296_cienqro"
-export const DB_PORT = process.env.DB_PORT || 3306
+export const DB_USER = process.env.DB_USER || "jonathan";
+export const DB_PASSWORD = process.env.DB_PASSWORD || "admin";
+export const DB_HOST = process.env.DB_HOST || "localhost";
+export const DB_DATABASE = process.env.DB_DATABASE || "cienqro";
+export const DB_PORT = process.env.DB_PORT || 3307;
 export const TOKEN_SECRET = process.env.TOKEN_SECRET || "d4ae3d57491eb9fd26db358f982200d4";
