3be57f7a6aff667a0ddff9cb7295dd704bb0745a
Jonathancraxker <Jonathancraxkerssj@gmail.com>
2025-11-19 18:43:10 -0600

    Se cambio la url de la api en el registro_citas.model para el QR

--- src/config/config.js
+++ src/config/config.js
@@ -6,8 +6,8 @@ export const PORT = process.env.PORT || 4000
 
 export const DB_USER = process.env.DB_USER || "jonathan"; //usuario
 export const DB_PASSWORD = process.env.DB_PASSWORD || "admin"; //contraseña
-export const DB_HOST = process.env.DB_HOST || "localhost"; //
-export const DB_DATABASE = process.env.DB_DATABASE || "ecoparking"; //ecoparking
+export const DB_HOST = process.env.DB_HOST || "localhost"; //host
+export const DB_DATABASE = process.env.DB_DATABASE || "ecoparking"; //db
 export const DB_PORT = process.env.DB_PORT || 3307; //puerto
 
 export const ACCESS_TOKEN_SECRET = process.env.ACCESS_TOKEN_SECRET || "d4ae3d57491eb9fd26db358f982200d4";
